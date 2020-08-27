f49efd4f939127a2f6dc2cd1eb64f900f020356f
Nayman Leung <naymansleung@gmail.com>
2020-08-26 21:48:38 -0400

    Updates

---
+++ precache-manifest.3f1f6c02cca5be468d5d3d95e5856d10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d90030d496d81298b66cb6baa932f6",
+    "url": "/Personal-Website/index.html"
+  },
+  {
+    "revision": "62dee71da10385ef8896",
+    "url": "/Personal-Website/static/css/main.a8034071.chunk.css"
+  },
+  {
+    "revision": "dc0f7491a79b62b12076",
+    "url": "/Personal-Website/static/js/2.cfe6a74f.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Personal-Website/static/js/2.cfe6a74f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62dee71da10385ef8896",
+    "url": "/Personal-Website/static/js/main.08ed2732.chunk.js"
+  },
+  {
+    "revision": "3b8518bb792bbe05f0d5",
+    "url": "/Personal-Website/static/js/runtime-main.390b94a5.js"
+  },
+  {
+    "revision": "ceccc5100eec48bcc0dfee8d3c2f3492",
+    "url": "/Personal-Website/static/media/kde_plasma_icecold2.ceccc510.png"
+  },
+  {
+    "revision": "2cd999542e62e0f8a828f051f507db93",
+    "url": "/Personal-Website/static/media/profile_icon-Copy.2cd99954.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Personal-Website/precache-manifest.23e2023b0de0c2b3a8a9f49bc941bba5.js"
+  "/Personal-Website/precache-manifest.3f1f6c02cca5be468d5d3d95e5856d10.js"
 );
 
 self.addEventListener('message', (event) => {
